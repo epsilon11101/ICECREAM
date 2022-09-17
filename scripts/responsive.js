a1308c8b801747595d63cd1de2abc8fd11433261
epsilon11101 <amirandav1991@gmail.com>
2022-09-17 04:02:51 -0500

    fixed error on responsive header girl image

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -1,11 +1,12 @@
 let girl_img = document.getElementsByClassName("hero-body__row_right_bg_girl");
 
-let variable = (width) => {
-  return width * 0.022;
+let map_value = (x, in_min, in_max, out_min, out_max) => {
+  return ((x - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
+
 onresize = () => {
   w = window.screen.width;
   h = window.screen.height;
-  console.log(w, h, variable(w) + "vh");
-  girl_img[0].style.left = `${variable(w)}vh`;
+
+  girl_img[0].style.left = `${map_value(w, 768, 1279, 18, 39)}vh`;
 };
