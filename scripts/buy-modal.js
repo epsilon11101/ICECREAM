5087c1972317e209047b2ce4542676359f0d3d6e
epsilon11101 <amirandav1991@gmail.com>
2022-09-19 02:14:41 -0500

    added close function tu form button

--- scripts/buy-modal.js
+++ scripts/buy-modal.js
@@ -1,8 +1,11 @@
 let buy_button = document.getElementById("buy-btn");
 let buy_modal = document.getElementsByClassName("buy-modal");
 let close_button = document.querySelector(".buy-modal  .fa-xmark");
+let send_button = document.querySelector(".buy-modal  button");
+
 let closeModal = () => buy_modal[0].classList.toggle("hidde-buy");
 
 console.log(close_button);
 buy_button.addEventListener("click", closeModal);
 close_button.addEventListener("click", closeModal);
+send_button.addEventListener("click", closeModal);
