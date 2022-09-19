5f1bba0b02ab0065ee58ea939f82d4968f39d09f
epsilon11101 <amirandav1991@gmail.com>
2022-09-19 15:54:59 -0500

    fixed bubbles error position and added indexnig

--- scripts/bubbles.js
+++ scripts/bubbles.js
@@ -1,9 +1,9 @@
 function createBubble() {
   const bub_section = document.getElementsByClassName("bubbles");
   const createElement = document.createElement("span");
-  var size = Math.random() * 80;
-  createElement.style.width = 20 + size + "px";
-  createElement.style.height = 20 + size + "px";
+  var size = Math.random() * 60;
+  createElement.style.width = 10 + size + "px";
+  createElement.style.height = 10 + size + "px";
   createElement.style.left = Math.random() * innerWidth + "px";
   bub_section[0].appendChild(createElement);
 
@@ -12,4 +12,4 @@ function createBubble() {
   }, 4000);
 }
 
-setInterval(createBubble, 50);
+setInterval(createBubble, 300);
