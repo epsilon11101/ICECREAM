0396f1debb0fa47eebef92cca6890109310298d2
epsilon11101 <amirandav1991@gmail.com>
2022-09-19 03:20:01 -0500

    changes names on testimonials

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,6 +2,7 @@ let buy_button = document.getElementById("buy-btn");
 let buy_modal = document.getElementsByClassName("buy-modal");
 let close_button = document.querySelector(".buy-modal  .fa-xmark");
 let send_button = document.querySelector(".buy-modal  button");
+let send_mobile_button = document.querySelector(".mobile-menu input");
 
 let closeModal = () => buy_modal[0].classList.toggle("hidde-buy");
 let closeModalLocation = () => location_modal[0].classList.toggle("hidde-buy");
@@ -9,6 +10,7 @@ let closeModalLocation = () => location_modal[0].classList.toggle("hidde-buy");
 buy_button.addEventListener("click", closeModal);
 close_button.addEventListener("click", closeModal);
 send_button.addEventListener("click", closeModal);
+send_mobile_button.addEventListener("click", closeModal);
 
 let location_button = document.getElementById("location_btn");
 let location_modal = document.getElementsByClassName("location-modal");
